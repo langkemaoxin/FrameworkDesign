6ff376095758b54f358dcb8cfd6f627050168b5b
RexChen <2363613998@qq.com>
2022-09-01 06:46:48 +0800

    Init

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FrameworkDesign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("FrameWork Design");
         }
     }
 }
